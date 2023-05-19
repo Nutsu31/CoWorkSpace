6dd2e9bc805b2a1aee796cd06aa511a98e938b43
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2023-05-20 03:16:22 +0400

    change setLogin to true

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRoute = () => {
-  const [logedIn, setLogedIn] = useState(false);
+  const [logedIn, setLogedIn] = useState(true);
 
   return <>{logedIn ? <Outlet /> : <Navigate to="/log-in" />}</>;
 };
