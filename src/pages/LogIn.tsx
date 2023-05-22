fd10e471d6885b865fc1afcbf68ab322102a0edf
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2023-05-23 02:01:02 +0400

    added animations on divs

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -5,6 +5,8 @@ import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
 import { baseURL } from "./Root";
+import { Blocks } from "react-loader-spinner";
+import AnimationSection from "components/motions/AnimationSection";
 
 interface UserDataType {
   status: number;
@@ -22,8 +24,10 @@ const LogIn = () => {
   const { register, handleSubmit, reset } = useForm();
   const [userData, setUserData] = useState<UserDataType>(initialValue);
   const navigate = useNavigate();
+  const [loader, setLoader] = useState(false);
 
   const onSubmit = handleSubmit((data) => {
+    setLoader(true);
     axios({
       method: "POST",
       url: baseURL + "/login",
@@ -35,8 +39,14 @@ const LogIn = () => {
         password: data.password,
       },
     })
-      .then((res) => setUserData(res.data))
-      .catch((err) => setUserData(err.response.data));
+      .then((res) => {
+        setUserData(res.data);
+        setLoader(false);
+      })
+      .catch((err) => {
+        setUserData(err.response.data);
+        setLoader(false);
+      });
     reset({
       email: "",
       password: "",
@@ -71,50 +81,63 @@ const LogIn = () => {
   }, [userData]);
 
   return (
-    <FormWrapper>
-      <form onSubmit={onSubmit}>
-        <FormControl
-          sx={{
-            width: { xs: 300, sm: 350, md: 400, lg: 450 },
-            height: 400,
-            padding: "60px 20px",
-            backgroundColor: "rgba(193, 193, 193, 1)",
-            display: "flex",
-            alignItems: "center",
-            gap: 2,
-            borderRadius: 2,
-          }}
-        >
-          <h1>შესვლა</h1>
-
-          <TextField
-            sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
-            label="ელ.ფოსტა"
-            type="email"
-            {...register("email", { required: true })}
-          />
-          <TextField
-            sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
-            label="პაროლი"
-            type="password"
-            {...register("password", { required: true })}
-          />
-          {userData.status >= 400 ? (
-            <WrongPass status={userData?.status}>{userData.user}</WrongPass>
-          ) : null}
-          <Button
-            sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
-            variant="contained"
-            type="submit"
+    <AnimationSection>
+      <FormWrapper>
+        <form onSubmit={onSubmit}>
+          <FormControl
+            sx={{
+              width: { xs: 300, sm: 350, md: 400, lg: 450 },
+              height: 400,
+              padding: "60px 20px",
+              backgroundColor: "rgba(193, 193, 193, 1)",
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+              borderRadius: 2,
+            }}
           >
-            შესვლა
-          </Button>
-          <Link style={{ color: "#3789e7" }} to="/join-us">
-            ახალი ანგარიშის შექმნა
-          </Link>
-        </FormControl>
-      </form>
-    </FormWrapper>
+            {loader ? (
+              <Blocks
+                visible={true}
+                height="64"
+                width="64"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+              />
+            ) : (
+              <h1>შესვლა</h1>
+            )}
+
+            <TextField
+              sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
+              label="ელ.ფოსტა"
+              type="email"
+              {...register("email", { required: true })}
+            />
+            <TextField
+              sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
+              label="პაროლი"
+              type="password"
+              {...register("password", { required: true })}
+            />
+            {userData.status >= 400 ? (
+              <WrongPass status={userData?.status}>{userData.user}</WrongPass>
+            ) : null}
+            <Button
+              sx={{ width: { xs: 200, sm: 250, md: 300, lg: 400 } }}
+              variant="contained"
+              type="submit"
+            >
+              შესვლა
+            </Button>
+            <Link style={{ color: "#3789e7" }} to="/join-us">
+              ახალი ანგარიშის შექმნა
+            </Link>
+          </FormControl>
+        </form>
+      </FormWrapper>
+    </AnimationSection>
   );
 };
 
